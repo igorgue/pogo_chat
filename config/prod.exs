4b777a8bdd94a6775a53a5cce25c207c90ae629d
Dainel Vera <arubinofaux@gmail.com>
2016-08-18 14:36:29 -0400

    remove url

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,6 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pogo_chat, PogoChat.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4001],
-  url: [host: "45.33.93.180", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
